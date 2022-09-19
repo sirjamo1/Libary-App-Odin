b0a049fb0ec56881cc3d2ca450fd98eea45f2341
Sirjamo1 <jam_how@hotmail.com>
2022-09-19 14:04:29 +1000

    added html css js

---
+++ object.js
@@ -0,0 +1,78 @@
+let myLibary = ["1977", "new moon", "art deco"];
+
+function Book() {
+   this.title = title
+   this.author = author
+}
+
+function addBookToLibary() {
+   let newBook = []
+  const title = document.getElementById("title").value;
+  
+
+}
+
+document.getElementById("books").innerHTML = myLibary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Student() {}
+
+// Student.prototype.sayName = function () {
+//     console.log(`${this.name} is cool ${this.grade}`);
+// };
+
+// function EighthGrader(name) {
+//     this.name = name;
+//     this.grade = 8;
+// }
+// function NinthGrader(name) {
+//  this.name = name;
+//  this.grade = 9;
+// }
+// NinthGrader.prototype = Object.create(Student.prototype)
+// EighthGrader.prototype = Object.create(Student.prototype);
+// const peter = new NinthGrader("peter")
+// const carl = new EighthGrader("carl");
+// carl.sayName(); 
+// peter.sayName()
+// console.log(carl.grade); // 8
+// console.log(peter.grade)
