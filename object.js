9b32602674329d00d7401e16d73e8cde38a347e7
Sirjamo1 <jam_how@hotmail.com>
2022-09-21 06:33:48 +1000

    added button to each book

--- object.js
+++ object.js
@@ -8,7 +8,6 @@ function Book(title, author, pages, read) {
     this.id = Math.floor(Math.random() * 10000000);
 }
 
-console.log(Math.floor(Math.random() * 10000000));
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
@@ -36,10 +35,10 @@ function deleteBook(selectedId) {
     const allBooks = document.querySelectorAll(".card");
     for (let i = 0; i < allBooks.length; i++) {
         let bookId = allBooks[i].getAttribute("id");
-      //   console.log(selectedId, parseInt(bookId));
+        //   console.log(selectedId, parseInt(bookId));
         if (selectedId === parseInt(bookId)) {
             console.log(selectedId, parseInt(bookId));
-              allBooks[i].remove();
+            allBooks[i].remove();
         }
     }
 }
@@ -50,9 +49,7 @@ function showBookForm() {
 function hideBookForm() {
     document.getElementById("book-form").style.display = "none";
 }
-function hello() {
-    console.log("hello");
-}
+
 function displayBooks() {
     const books = document.querySelector(".books");
     const deleteDivs = document.querySelectorAll(".card");
@@ -61,50 +58,41 @@ function displayBooks() {
     }
     myLibrary.forEach((myLibrary) => {
         const card = document.createElement("div");
+        books.appendChild(card);
         card.classList.add("card");
         card.setAttribute("id", myLibrary.id);
         const deleteBtn = document.createElement("button");
         deleteBtn.onclick = function () {
-            hello();
             deleteBook(myLibrary.id);
         };
         deleteBtn.classList.add("book-delete-btn");
-
         deleteBtn.textContent = "X";
         card.appendChild(deleteBtn);
-
-        console.log(myLibrary.id);
-        books.appendChild(card);
         for (let key in myLibrary) {
             console.log(`${key}: ${myLibrary[key]}`);
             const para = document.createElement("p");
+            if (key == "read") {
+            //   let btnText = myLibrary[key] == true ? "Change to Not yet" : "Change to Yes"
+               const readDiv = document.createElement("div")
+               readDiv.classList.add("readDiv")
+               card.appendChild(readDiv)
+               para.textContent = `${key}: ${myLibrary[key]}`;
+               readDiv.appendChild(para)
+               const readBtn = document.createElement("button");
+               readBtn.classList.add("readBtn")
+               readBtn.textContent =
+                   myLibrary[key] == true
+                       ? "Change to Not yet"
+                       : "Change to Yes";
+               readDiv.appendChild(readBtn)
+
+            } else {
             para.textContent = `${key}: ${myLibrary[key]}`;
             card.appendChild(para);
+            }
         }
     });
 }
 
 displayBooks();
 
-// function Student() {}
-
-// Student.prototype.sayName = function () {
-//     console.log(`${this.name} is cool ${this.grade}`);
-// };
-
-// function EighthGrader(name) {
-//     this.name = name;
-//     this.grade = 8;
-// }
-// function NinthGrader(name) {
-//  this.name = name;
-//  this.grade = 9;
-// }
-// NinthGrader.prototype = Object.create(Student.prototype)
-// EighthGrader.prototype = Object.create(Student.prototype);
-// const peter = new NinthGrader("peter")
-// const carl = new EighthGrader("carl");
-// carl.sayName();
-// peter.sayName()
-// console.log(carl.grade); // 8
-// console.log(peter.grade)
