ac9c33b9637b9c3c51a65a753259104ba4d06572
Sirjamo1 <jam_how@hotmail.com>
2022-09-20 12:53:42 +1000

    function to add new book

--- object.js
+++ object.js
@@ -1,99 +1,60 @@
-let myLibary = [];
-//Object contructor
+let myLibrary = [];
+//Object constructor
 function Book(title, author, pages, read) {
-   this.title = title;
-   this.author = author;
-   this.pages = pages;
-   this.read = read;
-
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibary(title, author, pages, read) {
-   let newBook = new Book(title, author, pages, read)
- myLibary.push(newBook)
- //console.log(newBook)
-
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayBooks();
 }
 
+function formData() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("true").checked ? true : false;
 
+    if (title == "" || author == "" || pages == "") {
+        return alert("please fill in all data");
+    } else {
+        addBookToLibrary(title, author, pages, read);
+        document.getElementById("book-form").reset();
+        hideBookForm();
+    }
+}
+
+function showBookForm() {
+    document.getElementById("book-form").style.display = "grid";
+}
+function hideBookForm() {
+    document.getElementById("book-form").style.display = "none";
+}
 function displayBooks() {
-   const books = document.querySelector(".books")
-   myLibary.forEach(myLibary => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-      books.appendChild(card);
-      for (let key in myLibary) {
-         console.log(`${key}: ${myLibary[key]}`);
-         const para = document.createElement("p");
-         para.textContent = (`${key}: ${myLibary[key]}`)
-         card.appendChild(para)
-      }
-   })
+    const books = document.querySelector(".books");
+    const deleteDivs = document.querySelectorAll(".card");
+    console.log(deleteDivs);
+    for (let i = 0; i < deleteDivs.length; i++) {
+        deleteDivs[i].remove();
+    }
+    myLibrary.forEach((myLibrary) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        books.appendChild(card);
+        for (let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`);
+            const para = document.createElement("p");
+            para.textContent = `${key}: ${myLibrary[key]}`;
+            card.appendChild(para);
+        }
+    });
 }
 
-const theHobbit = addBookToLibary("The Hobbit", "J.R.R. Tolkien", 289, false);
-const theHobbit2 = addBookToLibary(
-    "The Hobbit 2",
-    "J.R.R. Tolkien",
-    244,
-    false
-);
-const theHobbit3 = addBookToLibary("The Hobbit", "J.R.R. Tolkien", 289, false);
-const theHobbit4 = addBookToLibary(
-    "The Hobbit 2",
-    "J.R.R. Tolkien",
-    244,
-    false
-);
-const theHobbit5 = addBookToLibary("The Hobbit", "J.R.R. Tolkien", 289, false);
-const theHobbit6 = addBookToLibary(
-    "The Hobbit 2",
-    "J.R.R. Tolkien",
-    244,
-    false
-);
 displayBooks();
-// document.getElementById("books").innerHTML = myLibary[0].title
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // function Student() {}
 
@@ -113,7 +74,7 @@ displayBooks();
 // EighthGrader.prototype = Object.create(Student.prototype);
 // const peter = new NinthGrader("peter")
 // const carl = new EighthGrader("carl");
-// carl.sayName(); 
+// carl.sayName();
 // peter.sayName()
 // console.log(carl.grade); // 8
 // console.log(peter.grade)
